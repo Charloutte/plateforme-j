baf1c60c552ae2abd96dd76dd513c91845f656f9
Charloutte <32666955+Charloutte@users.noreply.github.com>
2018-11-10 12:58:21 +0100

    Update plateforme.py

--- plateforme.py
+++ plateforme.py
@@ -13,8 +13,16 @@ chemin = 'synthese-test.csv'
 
 f = open(chemin, 'r')
 
-# avec la fonction readline() et la boucle while on affiche chaque ligne du fichier csv
-line = f.readline()
-while line:
-    print(line)
-    line = f.readline()
+## avec la fonction readline() et la boucle while on affiche chaque ligne du fichier csv
+#line = f.readline()
+#while line:
+#    print(line)
+#    line = f.readline()
+
+
+try :
+    fichier_csv = csv.reader(f)
+    for row in fichier_csv :
+        print(row[0])
+finally :
+    f.close()
