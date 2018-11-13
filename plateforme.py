d5f7e5ddde3b0a7daefec9bd34df1425d9bfad95
Charloutte <32666955+Charloutte@users.noreply.github.com>
2018-11-13 22:37:20 +0100

    Update plateforme.py

--- plateforme.py
+++ plateforme.py
@@ -6,24 +6,31 @@ Created on Mon Nov  5 11:42:53 2018
 @author: CharlotteMorin
 """
 
-import csv
 
-chemin = 'synthese-test.csv'
-
-f = open(chemin, 'r')
-
-## avec la fonction readline() et la boucle while on affiche chaque ligne du fichier csv
-#line = f.readline()
-#while line:
-#    print(line)
-#    line = f.readline()
+#import csv
+#
+#with open('synthese-test.csv', newline='') as csvfile:
+#    spamreader = csv.reader(csvfile, delimiter=',')
+#    for row in spamreader:
+#        print(row)
 
 
-try :
-    fichier_csv = csv.DictReader(f)
-    
-    for row in fichier_csv :
-        print(row)
-        
-finally :
-    f.close()
+chemin = "synthese-test.csv"
+
+file = open(chemin, 'r', encoding='utf-8') 
+data = file.read()
+
+rows = data.split('\n')
+#print(type(rows))
+
+
+count=0
+
+final_data = []
+for row in rows :
+    split_list = row.split(';')
+    final_data.append(split_list)
+    count +=1
+
+final_data = final_data[1:count]
+#print(final_data)
